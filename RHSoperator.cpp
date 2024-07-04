bfd8f40e0fd24c5c4778ab8acf8f3b0f10f8a528
andreazor <99874984+andreazor@users.noreply.github.com>
2024-07-05 00:20:42 +0200

    Add files via upload

--- RHSoperator.cpp
+++ RHSoperator.cpp
@@ -1,3 +1,5 @@
+#include <vector> // Para std::vector
+#include <omp.h>  // Para OpenMP
 #include "RHSoperator.h"
 
 template<class T>
@@ -6,7 +8,6 @@ RHSOperator<T>::RHSOperator()
 
 }
 
-
 template<class T>
 RHSOperator<T>::~RHSOperator()
 {
@@ -14,12 +15,10 @@ RHSOperator<T>::~RHSOperator()
 }
 
 template<class T>
-Central1D<T>::Central1D(DataStruct<T> &_U, 
-                     DataStruct<T> &_mesh, 
-                     FluxFunction<T> &_F):
-U(_U), mesh(_mesh), F(_F)
+Central1D<T>::Central1D(DataStruct<T> &_U, DataStruct<T> &_mesh, FluxFunction<T> &_F)
+    : U(_U), mesh(_mesh), F(_F)
 {
-  RHS.setSize(_U.getSize());
+    RHS.setSize(_U.getSize());
 }
 
 template<class T>
@@ -31,50 +30,50 @@ Central1D<T>::~Central1D()
 template<class T>
 void Central1D<T>::evalRHS(DataStruct<T> &Uin)
 {
-  // the BC should be included in the mesh
-  // momentarily done here by hand
-  T *dataRHS = RHS.getData();
-  const T *dataU = Uin.getData();
-  const T *dataMesh = mesh.getData();
-  const int len = U.getSize();
-  
-  for(int j = 0; j < len; j++)
-  {
-    T dx;
-    if(j == 0)
-    {
-      dx = dataMesh[len-1] - dataMesh[len-2];
-      dx += dataMesh[1] - dataMesh[0];
-      dataRHS[0] = -(F.computeFlux(dataU[1]) - F.computeFlux(dataU[len-2]))/dx;
-    }
-    else
-    {
-      dx = dataMesh[j+1] - dataMesh[j-1];
-      dataRHS[j] = -(F.computeFlux(dataU[j+1]) - F.computeFlux(dataU[j-1]))/dx;
-    }
-  }
+    // The BC should be included in the mesh
+    // Momentarily done here by hand
+    T *dataRHS = RHS.getData();
+    const T *dataU = Uin.getData();
+    const T *dataMesh = mesh.getData();
+    const int len = U.getSize();
 
-  dataRHS[len-1] = dataRHS[0];
+    // Precalcular valores de flux para evitar recalculaciones
+    std::vector<T> fluxValues(len);
+    for (int i = 0; i < len; ++i)
+    {
+        fluxValues[i] = F.computeFlux(dataU[i]);
+    }
+
+    #pragma omp parallel for
+    for (int j = 1; j < len - 1; ++j)
+    {
+        T dx = dataMesh[j + 1] - dataMesh[j - 1];
+        dataRHS[j] = -(fluxValues[j + 1] - fluxValues[j - 1]) / dx;
+    }
+
+    // Calcular el primer y œltimo elemento fuera del bucle paralelizado
+    T dx0 = (dataMesh[len - 1] - dataMesh[len - 2]) + (dataMesh[1] - dataMesh[0]);
+    dataRHS[0] = -(fluxValues[1] - fluxValues[len - 2]) / dx0;
+    dataRHS[len - 1] = dataRHS[0]; // Aplicar condici—n de contorno
 }
 
 template<class T>
 void Central1D<T>::eval()
 {
-  evalRHS(U);
+    evalRHS(U);
 }
 
 template<class T>
 void Central1D<T>::eval(DataStruct<T> &Uin)
 {
-  evalRHS(Uin);
+    evalRHS(Uin);
 }
 
 template<class T>
 DataStruct<T>& Central1D<T>::ref2RHS()
 {
-  return RHS;
+    return RHS;
 }
-
 
 template class RHSOperator<float>;
 template class RHSOperator<double>;
--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 SOURCE_FILES="\
-main.cpp \
-DataStructs.cpp \
-rk4.cpp \
-FluxFunctions.cpp \
-RHSoperator.cpp"
+ main.cpp \
+ DataStructs.cpp \
+ rk4.cpp \
+ FluxFunctions.cpp \
+ RHSoperator.cpp"
 
-mpic++ -g -O3 -D_DOUBLE_ $SOURCE_FILES -Iincludes -o double.p
+mpic++ -g -O3 -pg -D_DOUBLE_ $SOURCE_FILES -Iincludes -o double.p
 
-mpic++ -g -O3 $SOURCE_FILES -Iincludes -o single.p
+mpic++ -g -O3 -pg $SOURCE_FILES -Iincludes -o single.p
